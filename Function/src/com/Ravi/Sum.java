5bbd39d1e0a7989c16727c8d6bf9abdf35f3a9cc
ravirj0601 <ravirj0601@gmail.com>
2022-02-03 23:34:39 +0530

    added some codes in function

--- Function/src/com/Ravi/Sum.java
+++ Function/src/com/Ravi/Sum.java
@@ -7,34 +7,30 @@ public class Sum {
         Scanner in = new Scanner(System.in);
         System.out.println("--press 1 for 2 no Sum--");
         System.out.println("--press 2 for 3 no Sum--");
+        System.out.println("--press 3 for Product of 2 no--");
         int choice = in.nextByte();
-        switch (choice) {
-            case 1 -> System.out.println("Sum of the Two no: " + sum());
-            case 2 -> System.out.println("Sum of three No: " + sum2(20, 30, 10));
-            default -> System.out.println("Wrong Choice");
-        }
-
-    }
-
-    private static int sum2(int a,int b,int c) {
-        Scanner in = new Scanner(System.in);
-        int num, num1,num2,sum;
-        System.out.print("Enter Three Numbers: -");
-        num = in.nextByte();
-        num1 = in.nextByte();
-        num2 = in.nextByte();
-        sum = num+num1+num2;
-        return sum;
-    }
-
-    private static int sum() {
-        Scanner in = new Scanner(System.in);
         int num, num1,sum;
         System.out.print("Enter The First No:- ");
         num = in.nextByte();
         System.out.print("Enter The Second  No:- ");
         num1 = in.nextByte();
-        sum = num+num1;
+        switch (choice) {
+            case 1 -> System.out.println("Sum of the Two no: " + sum(num,num1));
+//            case 2 -> System.out.println("Sum of three No: " + sum2(20, 30, 10));
+            case 3 -> System.out.println("Product of Two No: " + product(num,num1));
+            default -> System.out.println("Wrong Choice");
+        }
+
+    }
+
+    private static int product(int a,int b) {
+        int product = a * b;
+        return product;
+    }
+
+    private static int sum(int num,int num1) {
+
+        int sum = num+num1;
         return sum;
     }
 }
